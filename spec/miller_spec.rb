62e08e6466867de1744948c62eacd5ca9a059fdd
Maxim Kaschenko <mkaschenko@me.com>
2014-06-17 00:49:50 +0700

    "... whose only inheritance ... was his mill, his donkey, and his cat."
    * Tale::Miller#inheritance

--- spec/miller_spec.rb
+++ spec/miller_spec.rb
@@ -1,5 +1,7 @@
 require_relative '../tale/miller'
 
 describe Tale::Miller do
-
+  it 'has own inheritance' do
+    expect(subject.inheritance).to eq(['mill', 'donkey', 'cat'])
+  end
 end
--- tale/miller.rb
+++ tale/miller.rb
@@ -1,5 +1,7 @@
 module Tale
   class Miller
-
+    def inheritance
+      ['mill', 'donkey', 'cat']
+    end
   end
 end
