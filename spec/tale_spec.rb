96cd3ff41b8d34cdf1535ecfab6023f9b5ad7c2f
Maxim Kaschenko <mkaschenko@me.com>
2014-06-17 00:44:15 +0700

    Start the tale
    * Tale.start

--- spec/tale_spec.rb
+++ spec/tale_spec.rb
@@ -1,5 +1,7 @@
 require_relative '../tale'
 
 describe Tale do
-
+  it 'starts the tale' do
+    expect(subject.start).to be_truthy
+  end
 end
--- tale.rb
+++ tale.rb
@@ -1,3 +1,7 @@
-module Tale
+require_relative 'tale/miller'
 
+module Tale
+  def self.start
+    true
+  end
 end
