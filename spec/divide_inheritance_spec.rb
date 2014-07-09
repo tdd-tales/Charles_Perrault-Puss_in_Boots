0a5f9af9048295a601994b96a2051532f5923366
Maxim Kaschenko <mkaschenko@me.com>
2014-07-09 16:53:49 +0700

    Make Tale::DivideInheritance spec use Tale::Inheritance

--- spec/divide_inheritance_spec.rb
+++ spec/divide_inheritance_spec.rb
@@ -2,15 +2,18 @@ require_relative '../tale/divide_inheritance'
 require_relative '../tale/miller'
 
 describe Tale::DivideInheritance do
-  it 'runs the division' do
-    miller = Tale::Miller.new
-    the_eldest, the_second, the_youngest = miller.sons
+  describe '#run' do
+    let(:divide_inheritance) { described_class.new }
 
-    subject.run(miller.inheritance, miller.sons)
+    it 'divides an inheritance between sons' do
+      miller = Tale::Miller.new
+      the_eldest, the_second, the_youngest = miller.sons
 
-    expect(the_eldest.inheritance).to eq(['mill', 2])
-    expect(the_second.inheritance).to eq(['donkey', 1])
-    expect(the_youngest.inheritance).to eq(['cat', 0])
-    expect(miller.inheritance).to be_empty
+      divide_inheritance.run(miller.inheritance, miller.sons)
+
+      expect(the_eldest.inheritance).to eq(Tale::Inheritance.new(['mill', 2]))
+      expect(the_second.inheritance).to eq(Tale::Inheritance.new(['donkey', 1]))
+      expect(the_youngest.inheritance).to eq(Tale::Inheritance.new(['cat', 0]))
+    end
   end
 end
